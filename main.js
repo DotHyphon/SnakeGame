707e12ba73d04c1b84951b803aba6045eba56a1d
William Dean <William.dean93@gmail.com>
2023-04-03 18:55:07 +1000

    added object class and reformatted code

--- main.js
+++ main.js
@@ -10,15 +10,51 @@ const mainCtx = mainCanvas.getContext("2d");
 window.addEventListener("keydown", this.handleInput);
 
 let paused = false;
-const snakePosition = { x: 5, y: 15 };
-const snakeBody = [];
-let gameSpeed = 10;
-let snakeLength = 1;
-let snakeSpeed = 10;
-let snakeSize = 10;
-const snakeFood = { x: 0, y: 0, active: false };
 
-snakeBody.push({x: snakePosition.x, y: snakePosition.y});
+class Object {
+    constructor(x, y, size, color, active) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.color = color;
+        this.active = active;
+    }
+
+    get pos() {
+        return { x: this.x, y: this.y };
+    }
+
+    set pos(newPos) {
+        this.x = newPos.x;
+        this.y = newPos.y;
+    }
+
+    draw() {
+        if (this.active) {
+        mainCtx.fillStyle = this.color;
+        mainCtx.fillRect(this.x - this.size * 0.5, this.y - this.size * 0.5, this.size, this.size);
+        }
+    }
+}
+
+const snakeBody = [];
+let gameSpeed = 1;
+let snakeLength = 1;
+
+let snakeSize = 52;
+for (i = 0; i < snakeSize; i++) {
+    if (mainCanvas.width % snakeSize === 0) {
+        break;
+    }
+    snakeSize--;
+}
+
+const snake = new Object(0, 0, snakeSize, "rgb(0, 0, 0)", true);
+
+const snakeFood = new Object(0, 0, snakeSize, "rgb(150, 150, 150)", false);
+
+snakeBody.push(new Object(snake.x, snake.y, snake.size, snake.color, true));
+console.log(snakeBody[0]);
 
 let snakeDirection = Directions.Right;
 
@@ -26,74 +62,93 @@ update();
 
 
 function update() {
-    if (!paused) {
+    if (snake.x + snake.y == 0)
+    {
+        snake.pos = {x: snakeSize * 0.5, y: snakeSize * 0.5};
+    } else {
         switch (snakeDirection) {
             case Directions.Up:
-                snakePosition.y -= snakeSpeed;
+                snake.y -= snakeSize;
                 break;
             case Directions.Down:
-                snakePosition.y += snakeSpeed;
+                snake.y += snakeSize;
                 break;
             case Directions.Left:
-                snakePosition.x -= snakeSpeed;
+                snake.x -= snakeSize;
                 break;
             case Directions.Right:
-                snakePosition.x += snakeSpeed;
+                snake.x += snakeSize;
                 break;
         }
-
-        snakeBody.push({x: snakePosition.x, y: snakePosition.y});
-
-        if (snakeBody.length > snakeLength) {
-            snakeBody.shift();
-        }  
-
-        checkCollision();
-        
-        snakeFood.active ? null : spawnFood();
-
-        draw();
     }
-    setTimeout(() => { update(); }, 1000 / gameSpeed);
+
+    snakeBody.push(new Object(snake.x, snake.y, snake.size, snake.color, true));
+
+    if (snakeBody.length > snakeLength) {
+        snakeBody.shift();
+    }  
+        
+    snakeFood.active ? null : spawnFood();
+
+    checkCollision() ? null : draw();
+
+    //check if paused otherwise update
+    setTimeout(() => checkPaused() , 1000 / gameSpeed);
+}
+
+function checkPaused() {
+    paused ? setTimeout(() => checkPaused(), 1000) : update();
 }
 
 function spawnFood() {
-    snakeFood.x = Math.floor(Math.random() * mainCanvas.width / snakeSize) * snakeSize + 5;
-    snakeFood.y = Math.floor(Math.random() * mainCanvas.height / snakeSize) * snakeSize + 5;
+    snakeFood.x = Math.floor(Math.random() * mainCanvas.width / snakeSize) * snakeSize + snakeSize * 0.5;
+    snakeFood.y = Math.floor(Math.random() * mainCanvas.height / snakeSize) * snakeSize + snakeSize * 0.5;
+
+    snakeBody.forEach((bodyPart) => {
+        if (bodyPart.x === snakeFood.x && bodyPart.y === snakeFood.y) {
+            console.log('food spawned on snake');
+            spawnFood();
+        }
+    });
     snakeFood.active = true;
 }
 
 function checkCollision() {
-    if (snakePosition.x - snakeSize*0.5 < 0 || snakePosition.x + snakeSize*0.5 > mainCanvas.width) {
+    let collided = false;
+    if (snake.x - snakeSize*0.5 < 0 || snake.x + snakeSize*0.5 > mainCanvas.width) {
         console.log('hit left or right wall');
-        paused = true;
+        collided = true;
     }
-    else if (snakePosition.y - snakeSize*0.5 < 0 || snakePosition.y + snakeSize*0.5 > mainCanvas.height) {
+    else if (snake.y - snakeSize*0.5 < 0 || snake.y + snakeSize*0.5 > mainCanvas.height) {
         console.log('hit top or bottom wall');
-        paused = true;
+        collided = true;
     }
 
-    if (snakePosition.x === snakeFood.x && snakePosition.y === snakeFood.y) {
+    if (snake.x === snakeFood.x && snake.y === snakeFood.y) {
         snakeLength++;
         snakeFood.active = false;
     }
+
+    paused = collided;
+    return collided;
 }
 
 function draw() {
     //clear canvas
     mainCtx.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
 
-
     //draw snake
-    mainCtx.lineWidth = 2;
-    mainCtx.strokeStyle = "black";
     snakeBody.forEach((bodyPart, index) => {
-        mainCtx.fillRect(bodyPart.x - snakeSize*0.5, bodyPart.y - snakeSize*0.5, snakeSize, snakeSize)
+        snakeBody[index].draw();
     });
+    
 
     //draw snakefood
-    mainCtx.strokeStyle = "red";
-    snakeFood.active ? mainCtx.fillRect(snakeFood.x - snakeSize*0.5, snakeFood.y - snakeSize*0.5, snakeSize, snakeSize) : null;
+    snakeFood.draw();
+}
+
+function drawObject(object) {
+    mainCtx.fillRect(object.x - snakeSize*0.5, object.y - snakeSize*0.5, snakeSize, snakeSize);
 }
 
 function handleInput(e) {
@@ -116,10 +171,8 @@ function handleInput(e) {
         //reset defualt values
         paused = false;
         snakeBody.length = 0;
-        snakePosition.x = 5;
-        snakePosition.y = 15;
+        snake.pos(0, 0);
         snakeDirection = Directions.Right;
-        console.log(snakePosition);
     }
 }
 
